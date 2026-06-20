bc6ed7e02fe61d1fd48b5e74f51386030937d646
imransaadullah <48366039+imransaadullah@users.noreply.github.com>
2026-06-20 12:41:28 +0100

    Initial Commit

---
+++ main.py
@@ -0,0 +1,12 @@
+a = 0
+b = 50
+
+for i in range(a, b+1):
+    if i % 15 == 0:
+        print('FOOBAR')
+    elif i % 5 == 0:
+        print('BAR')
+    elif i % 3 == 0:
+        print('FOO')
+    else:
+        print(i)
